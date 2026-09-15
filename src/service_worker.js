0065a672f61c775c0e9201409a56973d2d2337e8
HackTestes <caio.hacktestes@gmail.com>
2026-09-15 12:36:35 -0300

    Cache update logic is now all on the service worker, the main pages only triggers it through PostMessage

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,6 +1,39 @@
-const version = "1"
+const version = "10"
 const main_page = "./tts.html"
 
+// Not needed for now
+function ArrayEquals(array_a, array_b)
+{
+    if ( Array.isArray(array_a) || Array.isArray(array_b) )
+    {
+        throw new Error("Type error, not an array");
+    }
+
+    // If any of them are undefined, return as if they aren't the same
+    // This is valid even if they are both undefined
+    if (array_a == undefined || array_b == undefined)
+    {
+        return false
+    }
+
+    // Fast comparison
+    // If their sizes are different, they aren't equal at all
+    if (array_a.length != array_b.length)
+    {
+        return false;
+    }
+
+    // If they have the same size, then we will check each element
+    for (let i = 0; i < array_a.length; ++i)
+    {
+        if (array_a[i] != array_b[i])
+        {
+            return false
+        }
+    }
+
+    return true
+}
 
 self.addEventListener('install', event =>
 {
@@ -35,19 +68,56 @@ async function HandleFetch(event)
     console.log(`Network responded`);
 
     // The network got something
-    
-    /*
-    // Only update the cache if it is different
-    if (cache_response.bytes() != network_response.clone().bytes())
-    {
-        event.waitUntil( cache.put(event.request, network_response.clone()) );
-    }
-    */
     event.waitUntil( cache.put(event.request, network_response.clone()) );
     return network_response;
 }
 
 self.addEventListener('fetch', event =>
 {
+    console.log('Fetch event');
     event.respondWith( HandleFetch(event) );
+});
+
+async function HandleMessages(event)
+{
+    console.log(`Message received! ${event.data}`);
+    const message = event.data;
+
+    if (message.operation = "update-cache")
+    {
+        console.log("Manual cache update triggered");
+
+        const cache = await caches.open('web-tts-pages');
+
+        const cache_response = await cache.match(main_page);
+
+        const network_response = await fetch(main_page,
+        {
+            signal: AbortSignal.timeout(10000),
+            headers: {
+                'cache': 'no-cache',
+            }
+        });
+
+        // Compare the results
+        if ( await cache_response.clone().text() != await network_response.clone().text() )
+        {
+            // They aren't the same, then we update the cache
+            await cache.add(main_page, network_response.clone());
+            event.source.postMessage({operation: "update-cache", status: "updated"})
+        }
+        else
+        {
+            event.source.postMessage({operation: "update-cache", status: "no-update-available"})
+        }
+    }
+    else
+    {
+        throw new Error("Invalid message oparation");
+    }
+}
+
+addEventListener("message", (event) =>
+{
+    HandleMessages(event);
 });
